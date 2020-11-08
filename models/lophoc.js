fad85c18a88429056374326cc95ee35582ef9912
unknown <lp.duy2k@gmail.com>
2020-11-08 15:52:14 +0700

    new

--- models/lophoc.js
+++ models/lophoc.js
@@ -1,6 +1,7 @@
 var lophoc = function () {
     this.danhSachSinhVien = [];
     this.moTa = 'Day la Duy';
+    this.tenLopHoc = 'FE4';
     this.LayDanhSachSinhVien = function () {
         return this.danhSachSinhVien;
     }
