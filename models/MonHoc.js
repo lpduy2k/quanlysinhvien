81befe48ab5ec84a9b61d64545611dbf1a855bdc
unknown <au.nguyen272@gmail.com>
2020-11-08 16:17:11 +0700

    quanlymonhoc

---
+++ models/MonHoc.js
@@ -0,0 +1,8 @@
+var MonHon = function () {
+    this.maMonHoc = '';
+    this.tenMonHoc = '';
+    this.xuatThongTinMonHoc = function () {
+        console.log('Mã môn học: ${this.maMonHoc}');
+        console.log('Tên môn học: ${this.tenMonHoc}');
+    }
+}
