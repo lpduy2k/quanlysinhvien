29da9f150e4461565a0c4a80b87beed7aae7ffb0
unknown <au.nguyen272@gmail.com>
2020-11-08 15:47:12 +0700

    them_tenLopHoc_LopHoc.js

---
+++ controllers/LopHoc.js
@@ -0,0 +1,7 @@
+var LopHoc = function () {
+    this.danhSachHocVien = [];
+    this.tenLopHoc = 'FrontEnd54';
+    this.layDanhSachHocVien = function () {
+        return this.danhSachHocVien
+    }
+}
