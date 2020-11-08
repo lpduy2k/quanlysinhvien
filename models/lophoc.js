9c8e3f8e81ec39ef0690b5d59fad72c3a990a134
unknown <lp.duy2k@gmail.com>
2020-11-08 15:47:04 +0700

    userA_commit

--- models/lophoc.js
+++ models/lophoc.js
@@ -1,5 +1,6 @@
 var lophoc = function () {
     this.danhSachSinhVien = [];
+    this.moTa = 'Day la Duy';
     this.LayDanhSachSinhVien = function () {
         return this.danhSachSinhVien;
     }
